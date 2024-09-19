a8e5a81825ff7758fc478636e5c10b494e41c8e1
M-Mikos <mikos.marcin.m@gmail.com>
2024-09-19 12:05:23 +0200

    Add populate-social script, update button template

--- src/scripts/populate-socials.js
+++ src/scripts/populate-socials.js
@@ -4,9 +4,53 @@
  * Steps:
  * 1. Read and parse content from "data/social-profiles.json"
  * 2. Read button template markup
- * 2. Read index.html markup from "src/index.html" as text value
- * 2. Declare output string
- * 3. For every data entry, append social button markup to output string with correct data
- * 4. Replace "%social-buttons&" placeholder with output string in index.html markup
- * 5. Save updated markup as index.html
+ * 3. Read app template markup
+ * 4. Declare social-buttons markup string
+ * 5. For every data entry, append social button markup to output string with correct data
+ * 6. Replace "%social-buttons&" placeholder with output string in index.html markup
+ * 7. Save updated markup as index.html
  */
+
+const fs = require("fs");
+
+// Read data and templates
+
+const dataJSON = fs.readFileSync(
+  `${__dirname}/../../data/social-profiles.json`,
+  "utf-8"
+);
+const links = JSON.parse(dataJSON).links;
+
+const templateApp = fs.readFileSync(
+  `${__dirname}/../templates/template-app.html`,
+  "utf-8"
+);
+const templateButton = fs.readFileSync(
+  `${__dirname}/../templates/template-button.html`,
+  "utf-8"
+);
+
+// Fill button templates
+
+const socialButtons = links.map((link) =>
+  templateButton
+    .replace(/{%URL%}/g, link.url)
+    .replace(/{%LABEL%}/g, link.label)
+);
+
+const socialButtonsMarkup = socialButtons.join(" ");
+
+// Fill app template
+
+const appMarkup = templateApp.replace(
+  /{%SOCIAL_BUTTONS%}/g,
+  socialButtonsMarkup
+);
+
+// Save output
+
+fs.writeFileSync(
+  `${__dirname}/../../index.html`,
+  appMarkup,
+  "utf-8"
+);
